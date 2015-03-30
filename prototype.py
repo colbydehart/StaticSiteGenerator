47451b333ea61ab204bc772896c879a498184453
Colby DeHart <colbydehart@gmail.com>
2015-03-29 19:38:44 -0500

    added pages

--- prototype.py
+++ prototype.py
@@ -1,6 +1,9 @@
+import os
 import sys
 
 from django.conf import settings
+
+BASE_DIR = os.path.dirname(__file__)
 
 settings.configure(
     DEBUG=True,
@@ -13,6 +16,7 @@ settings.configure(
         'sitebuilder',
     ),
     STATIC_URL='/static/',
+    SITE_PAGES_DIRECTORY=os.path.join(BASE_DIR, 'pages')
 )
 
 if __name__ == "__main__":
